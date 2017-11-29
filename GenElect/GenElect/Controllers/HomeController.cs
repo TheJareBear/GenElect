49b476b39f9ef613347c731cb1d1e9487c99efd5
Jarod Keene <jarod.keene@live.com>
2017-11-29 12:38:04 -0600

    Some work completed on the front end to make the site look specific to GenElect

--- GenElect/GenElect/Controllers/HomeController.cs
+++ GenElect/GenElect/Controllers/HomeController.cs
@@ -15,15 +15,16 @@ namespace GenElect.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            return View();
+        }
 
+        public ActionResult Electives()
+        {
             return View();
         }
     }
