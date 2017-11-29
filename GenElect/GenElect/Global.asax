ed80e4e97a3021607d1ba7addb673891f3f0ddda
Jarod Keene <jarod.keene@live.com>
2017-11-29 12:09:31 -0600

    This is the first commit

---
+++ GenElect/GenElect/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GenElect.MvcApplication" Language="C#" %>
---
+++ GenElect/GenElect/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GenElect.Startup))]
+namespace GenElect
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
